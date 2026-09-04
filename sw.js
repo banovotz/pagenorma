d488ffdbfde41db57e4d083bbb33a6d3adf7767b
Davor Banovic <davor.banovic@gmail.com>
2026-09-04 19:00:14 +0200

    Back to manual service worker

---
+++ sw.js
@@ -0,0 +1,46 @@
+const CACHE_NAME = 'mojih1500-v5';
+const ASSETS = [
+  './',
+  './index.html',
+  './src/styles/style.css',
+  './src/app.js',
+  './manifest.json',
+  './public/icon-192.png',
+  './public/icon-512.png'
+];
+
+// Instalacija Service Workera i spremanje datoteka u cache
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Aktivacija i čišćenje starih verzija cachea
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Dohvaćanje resursa: Prvo traži u Cacheu, ako nema - ide na Mrežu
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cachedResponse) => {
+      return cachedResponse || fetch(e.request);
+    })
+  );
+});
+
+let trenutniProjektId = null; // ID projekta kojem dodajemo unos
+
